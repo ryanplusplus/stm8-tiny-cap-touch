924a64b160b2b72122afe366bc9993d1dce460e3
Ryan Hartlage <ryanplusplus@gmail.com>
2022-01-10 18:01:27 -0500

    Updated tools

--- src/main.c
+++ src/main.c
@@ -7,34 +7,26 @@
 #include <stddef.h>
 #include "stm8s.h"
 #include "clock.h"
+#include "interrupts.h"
 #include "tim4_system_tick.h"
 #include "tiny_timer.h"
 #include "watchdog.h"
 #include "cap_touch.h"
 
 static tiny_timer_group_t timer_group;
-static tiny_timer_t timer;
-
-static void kick_watchdog(tiny_timer_group_t* timer_group, void* context) {
-  (void)context;
-  watchdog_kick();
-  tiny_timer_start(timer_group, &timer, 1, kick_watchdog, NULL);
-}
 
 void main(void) {
-  disableInterrupts();
+  interrupts_disable();
   {
-    watchdog_init();
     clock_init();
     tiny_timer_group_init(&timer_group, tim4_system_tick_init());
+    watchdog_init(&timer_group);
     cap_touch_init(&timer_group);
   }
-  enableInterrupts();
-
-  kick_watchdog(&timer_group, NULL);
+  interrupts_enable();
 
   while(true) {
     tiny_timer_group_run(&timer_group);
-    wfi();
+    interrupts_wait_for_interrupt();
   }
 }
